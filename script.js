622f7edbe0e1d6748dd8c61d82cdcec3a7b77efb
Muller <bmuller@comprovei.com>
2025-03-18 15:10:33 -0300

    Primeiro commit sistema ja funcionando

---
+++ script.js
@@ -0,0 +1,23 @@
+let total1 = 0;
+let total2 = 0;
+
+function adicionarPontos() {
+    let pontosJ1 = parseInt(document.getElementById('pontos1').value) || 0;
+    let pontosJ2 = parseInt(document.getElementById('pontos2').value) || 0;
+
+    total1 += pontosJ1;
+    total2 += pontosJ2;
+
+    document.getElementById('total1').innerText = total1;
+    document.getElementById('total2').innerText = total2;
+
+    document.getElementById('pontos1').value = '';
+    document.getElementById('pontos2').value = '';
+}
+
+function resetarPontos() {
+    total1 = 0;
+    total2 = 0;
+    document.getElementById('total1').innerText = total1;
+    document.getElementById('total2').innerText = total2;
+}
