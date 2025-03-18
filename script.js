1905167fff139a2745201ac3e481c1ec473542c8
bryan-Muller12 <brysn.ms@gmail.com>
2025-03-18 15:55:36 -0300

    alteração no design, e adição da diferença

--- script.js
+++ script.js
@@ -8,8 +8,11 @@ function adicionarPontos() {
     total1 += pontosJ1;
     total2 += pontosJ2;
 
+    let diferenca = Math.abs(total1 - total2); // Calcula a diferença absoluta
+
     document.getElementById('total1').innerText = total1;
     document.getElementById('total2').innerText = total2;
+    document.getElementById('diferenca').innerText = ` ${diferenca}`;
 
     document.getElementById('pontos1').value = '';
     document.getElementById('pontos2').value = '';
@@ -18,6 +21,8 @@ function adicionarPontos() {
 function resetarPontos() {
     total1 = 0;
     total2 = 0;
+    
     document.getElementById('total1').innerText = total1;
     document.getElementById('total2').innerText = total2;
+    document.getElementById('diferenca').innerText = "0"; // Reset da diferença
 }
